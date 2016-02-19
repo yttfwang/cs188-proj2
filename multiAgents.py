4543b19fccca58430c5f6929380dd10ac1310c3d
Yuting Wang <yttfwang@berkeley.edu>
2016-02-19 12:21:28 -0800

    finished everything except for P3

--- multiAgents.py
+++ multiAgents.py
@@ -107,6 +107,7 @@ def scoreEvaluationFunction(currentGameState):
       (not reflex agents).
     """
     #print currentGameState.getScore()
+    # print "GETSCORE"
     return currentGameState.getScore()
 
 class MultiAgentSearchAgent(Agent):
@@ -249,7 +250,120 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
           Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        finalLevel = gameState.getNumAgents() * self.depth 
+
+        def alphabeta(game_state, level, bestMin, bestMax):
+          agentIndex = level % gameState.getNumAgents()
+          if level >= finalLevel:
+            # print "Error with minimax function: levels_left should not be <= 0."
+            return None
+          
+          elif level == finalLevel - 1: # must be a ghost, minimizer
+            # print "IS FINAL DEPTH"
+            bestAction = ''
+            lowestEvalNum = float("inf")
+            #evalNum = -999999
+            
+            for action in game_state.getLegalActions(agentIndex):
+              # print "lowest level", action
+              newState = game_state.generateSuccessor(agentIndex, action)
+              
+
+              evalNum = self.evaluationFunction(newState)
+              # print evalNum
+              if evalNum < lowestEvalNum:
+                lowestEvalNum = evalNum
+                
+                bestAction = action
+              # if evalNum > bestMax:
+              #   bestMax = evalNum
+              # if evalNum < bestMin:
+              #   bestMin = evalNum
+            #pdb.set_trace()
+            #print "@@@1: level: ", level, ";  agentIndex: ", agentIndex, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@2: best eval, action: ", lowestEvalNum, bestAction
+            # print "lowest eval at last level", lowestEvalNum
+            return (lowestEvalNum, bestAction)
+          
+          elif level % gameState.getNumAgents() == 0: #Pacman's turn, maximizer
+            bestAction = 'Stop'
+            highestEvalNum = float("-inf")
+            #evalNum = -999999
+            
+            for action in game_state.getLegalActions(agentIndex):
+              # print action
+              newState = game_state.generateSuccessor(agentIndex, action)
+              
+              if newState.isWin():
+                # print "IS WIN"
+                evalNum = self.evaluationFunction(newState)
+                # print evalNum
+              elif newState.isLose():
+                # print "IS LOSE", action
+                evalNum = self.evaluationFunction(newState)
+                # print evalNum
+              else:
+                
+                evalNum , _  = alphabeta(newState, level + 1, bestMin, bestMax)
+                # print evalNum
+
+              if evalNum > highestEvalNum:
+                highestEvalNum = evalNum
+                bestAction = action
+              # print "bestmin", bestMin
+              if evalNum > bestMin:
+                return (highestEvalNum, bestAction)
+              bestMax = max(bestMax, highestEvalNum)
+              # print "bestMax, highestEvalNum", bestMax, highestEvalNum
+            #pdb.set_trace()
+            #print "@@@ 3: level: ", level, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@ 4: highest eval, action: ", highestEvalNum, bestAction
+            return (highestEvalNum, bestAction)
+
+          else: #Ghosts' turn, minimizer
+            bestAction = 'Stop'
+            lowestEvalNum = float("inf")
+            #evalNum = 999999
+            
+            for action in game_state.getLegalActions(agentIndex):
+              newState = game_state.generateSuccessor(agentIndex, action)
+              # print action
+              # if newState.isWin() or newState.isLose():
+              #   print "IS WINLOSE"
+              #   evalNum = self.evaluationFunction(newState)
+              if newState.isWin():
+                # print "IS WIN"
+                evalNum = self.evaluationFunction(newState)
+                # print evalNum
+              elif newState.isLose():
+                # print "IS LOSE", action
+                evalNum = self.evaluationFunction(newState)
+                # print evalNum
+              else:
+                #pdb.set_trace()
+                evalNum , _  = alphabeta(newState, level + 1, bestMin, bestMax)
+                # print evalNum
+
+              if evalNum < lowestEvalNum:
+                lowestEvalNum = evalNum
+                bestAction = action
+              # print "bestmax", bestMax
+              if evalNum < bestMax:
+                return (lowestEvalNum, bestAction)
+              bestMin = min(bestMin, lowestEvalNum)
+              # print "bestMin, lowestEvalNum", bestMin, lowestEvalNum
+            
+            #pdb.set_trace()
+            #print "@@@  5: level: ", level, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@  6: lowest eval, action: ", lowestEvalNum, bestAction
+            return (lowestEvalNum, bestAction)
+
+
+    
+
+        evalNum , action = alphabeta(gameState, 0, float("inf"), float("-inf")) #initially starts the level at 0
+        # print "evalnum is:", evalNum
+        return action
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -264,7 +378,112 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
           legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        finalLevel = gameState.getNumAgents() * self.depth 
+
+        def expectimax(game_state, level):
+          # Returns a tuple: (bestEvalNum, action)
+          # bestEvalNum is the evalNum at this level, with this game_state
+          # action is the action to take right now to get that bestEvalNum
+
+          agentIndex = level % gameState.getNumAgents()
+
+          if level >= finalLevel:
+            print "Error with minimax function: levels_left should not be <= 0."
+            return None
+          
+          elif level == finalLevel - 1: # must be a ghost, random action chooser!
+            #bestAction = 'Stop'
+            #lowestEvalNum = 999999
+            #evalNum = -999999
+            sumEvalNums = 0.0 # making it a float 
+            actionsList = game_state.getLegalActions(agentIndex)
+
+            for action in actionsList:
+              newState = game_state.generateSuccessor(agentIndex, action)
+              evalNum = self.evaluationFunction(newState)
+              sumEvalNums += evalNum
+            
+            #pdb.set_trace()
+            #print "@@@1: level: ", level, ";  agentIndex: ", agentIndex, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@2: best eval, action: ", lowestEvalNum, bestAction
+            return (sumEvalNums / len(actionsList), '') # returning a blank action because the action doesn't matter
+          
+          elif level % gameState.getNumAgents() == 0: #Pacman's turn, maximizer
+            bestAction = 'Stop'
+            highestEvalNum = -999999
+            #evalNum = -999999
+            
+            for action in game_state.getLegalActions(agentIndex):
+              newState = game_state.generateSuccessor(agentIndex, action)
+              
+              if newState.isWin() or newState.isLose():
+                evalNum = self.evaluationFunction(newState)
+              else:
+                #pdb.set_trace()
+                evalNum , _ = expectimax(newState, level + 1)
+
+              if evalNum > highestEvalNum:
+                highestEvalNum = evalNum
+                bestAction = action
+            
+            #pdb.set_trace()
+            #print "@@@ 3: level: ", level, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@ 4: highest eval, action: ", highestEvalNum, bestAction
+            return (highestEvalNum, bestAction)
+
+          else: #Ghosts' turn, random action chooser!
+            #bestAction = 'Stop'
+            #lowestEvalNum = 999999
+            #evalNum = 999999
+
+            sumEvalNums = 0.0 # making it a float 
+            actionsList = game_state.getLegalActions(agentIndex)
+
+            for action in actionsList:
+              newState = game_state.generateSuccessor(agentIndex, action)
+              
+              if newState.isWin() or newState.isLose():
+                evalNum = self.evaluationFunction(newState)
+              else:
+                #pdb.set_trace()
+                evalNum , _ = expectimax(newState, level + 1)
+
+              sumEvalNums += evalNum
+            
+            #pdb.set_trace()
+            #print "@@@1: level: ", level, ";  agentIndex: ", agentIndex, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@2: best eval, action: ", lowestEvalNum, bestAction
+            return (sumEvalNums / len(actionsList), '') # returning a blank action because the action doesn't matter
+
+
+
+            # for reference:
+            for action in game_state.getLegalActions(agentIndex):
+              newState = game_state.generateSuccessor(agentIndex, action)
+              
+              if newState.isWin() or newState.isLose():
+                evalNum = self.evaluationFunction(newState)
+              else:
+                #pdb.set_trace()
+                evalNum , _ = minimax(newState, level + 1)
+
+              if evalNum < lowestEvalNum:
+                lowestEvalNum = evalNum
+                bestAction = action
+            
+            #pdb.set_trace()
+            #print "@@@  5: level: ", level, ";  actions list: ", game_state.getLegalActions(agentIndex) 
+            #print "@@@  6: lowest eval, action: ", lowestEvalNum, bestAction
+            return (lowestEvalNum, bestAction)
+
+
+
+
+        #pdb.set_trace()
+
+        evalNum , action = expectimax(gameState, 0) #initially starts the level at 0
+        #print "@@@0 evalnum is:", evalNum
+        return action
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -274,7 +493,62 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    #successorGameState = currentGameState.generatePacmanSuccessor(action)
+    pos = currentGameState.getPacmanPosition()
+    food = currentGameState.getFood()
+    capsules = currentGameState.getCapsules() # capsules aka power pellets
+    ghostStates = currentGameState.getGhostStates()
+    numAgents = currentGameState.getNumAgents()
+    minFoodDist = 1E9
+    minCapsuleDist = 1E9
+    minGhostDist = 1E9
+
+
+    #for food
+    for x in range(food.width):
+      for y in range(food.height):
+        if food[x][y]:
+          foodManDist = abs(x - pos[0]) + abs(y - pos[1])
+          
+          if foodManDist < minFoodDist:
+            minFoodDist = foodManDist
+    if minFoodDist != 0:
+      invMinFoodDist = 1.0 / minFoodDist
+    else: 
+      invMinFoodDist = 1.0 / 1
+
+    # for capsules
+    for capsulePos in capsules:
+      capsuleManDist = abs(capsulePos[0] - pos[0]) + abs(capsulePos[1] - pos[1])
+      if capsuleManDist < minCapsuleDist:
+        minCapsuleDist = capsuleManDist
+    if minCapsuleDist != 0:
+      invMinCapsuleDist = 1.0 / minCapsuleDist
+    else: 
+      invMinCapsuleDist = 1.0 / 1
+
+    #for ghosts
+    for i in range(1, numAgents):
+      ghostPos = currentGameState.getGhostPosition(i)
+      ghostManDist = abs(ghostPos[0] - pos[0]) + abs(ghostPos[1] - pos[1])
+      if ghostManDist < minGhostDist:
+        minGhostDist = ghostManDist
+    if minGhostDist !=0:
+      invMinGhostDist = 1.0 / minGhostDist
+    else:
+      invMinGhostDist = 1.0 / 1
+   
+
+    if minGhostDist < 3:
+      invMinGhostDist = invMinGhostDist * 100
+    if minCapsuleDist < 3:
+      invMinCapsuleDist = invMinCapsuleDist * 100
+
+    
+
+    totalScore = currentGameState.getScore() + invMinFoodDist - invMinGhostDist + invMinCapsuleDist - len(capsules)*1000
+    
+    return totalScore
 
 # Abbreviation
 better = betterEvaluationFunction
